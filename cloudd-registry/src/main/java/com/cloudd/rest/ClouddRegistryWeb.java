b92ca4c3275861d9e0333a71ca986ed711256414
Belal Ansari <belalansari23@gmail.com>
2017-01-25 20:51:26 +0530

    updated

---
+++ cloudd-registry/src/main/java/com/cloudd/rest/ClouddRegistryWeb.java
@@ -0,0 +1,16 @@
+package com.cloudd.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/registry")
+public class ClouddRegistryWeb {
+
+	@GetMapping("/get")
+	public String getRegister()
+	{
+		return "Hello, calling get API.";
+	}
+}
