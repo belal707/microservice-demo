25b943bdd3b9954607a9543a2fca0eb443cd4964
Belal Ansari <belalansari23@gmail.com>
2017-01-25 13:22:24 +0530

    initial commit

---
+++ cloudd-registry/src/main/java/com/cloudd/ClouddRegistryApplication.java
@@ -0,0 +1,12 @@
+package com.cloudd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClouddRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClouddRegistryApplication.class, args);
+	}
+}
